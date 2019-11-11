fd48adab9d5a05ddae515cc0790e47e27b484c5d
@FullZero <fullzero5@gmail.com>
2019-11-11 22:12:53 +0300

    worck in api.weatherstack.com
    get  current weather

--- src/functions/weather-api.js
+++ src/functions/weather-api.js
@@ -7,6 +7,25 @@ export const useWeatherApi = () => {
     loading: true,
     errored: false
   });
-
+  const params = {
+    access_key: "YOUR_ACCESS_KEY",
+    query: "New York"
+  };
+  axios
+    .get("https://api.weatherstack.com/current", { params })
+    .then(response => {
+      const apiResponse = response.data;
+      state.weather = apiResponse;
+      console.log(
+        `Current temperature in ${apiResponse.location.name} is ${
+          apiResponse.current.temperature
+        }℃`
+      );
+    })
+    .catch(error => {
+      console.log(error);
+      state.errored = true;
+    })
+    .finally(() => (state.loading = false));
   return state;
 };
